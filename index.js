e19656e93ab09618c8dbf89aa90bdf83fc95f236
kennyho <kennyho.code@gmail.com>
2020-07-31 19:09:55 -0400

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+exports.books = [
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowlings',
+    },
+    {
+        title: 'Jurassic Park',
+        author: 'Michael Crichton',
+    },
+];
+
---
+++ src/books.js
@@ -0,0 +1,12 @@
+const books = [
+    {
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowlings',
+    },
+    {
+        title: 'Jusrassicasdasddadfasdfdd Park',
+        author: 'Michael Crichton',
+    },
+];
+
+export default books;
